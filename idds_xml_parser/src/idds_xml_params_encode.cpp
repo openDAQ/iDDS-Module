b0e47a3f7b8a005e6d9b82e15ca00ea761ec7b28
Daniel Barros <daniel.duraesdebarros@hbkworld.com>
2025-02-24 12:53:56 +0000

    [openDAQ] Fix PR comments

--- idds_xml_parser/src/idds_xml_params_encode.cpp
+++ idds_xml_parser/src/idds_xml_params_encode.cpp
@@ -2,319 +2,318 @@
 #include <cstdint>
 #include <vector>
 #include <string>
+#include <sstream>
 
 template<>
 std::string idds_xml_params_encode<int8_t>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-    strParam += "<value><Byte>" + std::to_string(m_Param) + "</Byte></value>";
-    strParam += "</param>";
-    return strParam;
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><Byte>" << std::to_string(m_Param) << "</Byte></value>";
+    ss << "</param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<std::vector<int8_t>>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-
-    strParam += "<value><ListOfByte>";
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><ListOfByte>";
     for (const auto& e: m_Param)
     {
-        std::string innerStr = "<Byte>" + std::to_string(e) + "</Byte>";
-         strParam += innerStr;
+        ss << "<Byte>" << std::to_string(e) << "</Byte>";
     }
-    strParam += "</ListOfByte></value></param>";
-    return strParam;
+    ss << "</ListOfByte></value></param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<uint8_t>::get_parsed_string() const
 {
-
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-    strParam += "<value><SByte>" + std::to_string(m_Param) + "</SByte></value>";
-    strParam += "</param>";
-    return strParam;
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><SByte>" << std::to_string(m_Param) << "</SByte></value>";
+    ss << "</param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<std::vector<uint8_t>>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-
-    strParam += "<value><ListOfSByte>";
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><ListOfSByte>";
     for (const auto& e: m_Param)
     {
-        std::string innerStr = "<SByte>" + std::to_string(e) + "</SByte>";
-         strParam += innerStr;
+        ss << "<SByte>" << std::to_string(e) << "</SByte>";
     }
-    strParam += "</ListOfSByte></value></param>";
-    return strParam;
+    ss << "</ListOfSByte></value></param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<int16_t>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-    strParam += "<value><Int16>" + std::to_string(m_Param) + "</Int16></value>";
-    strParam += "</param>";
-    return strParam;
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><Int16>" << std::to_string(m_Param) << "</Int16></value>";
+    ss << "</param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<std::vector<int16_t>>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-
-    strParam += "<value><ListOfInt16>";
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><ListOfInt16>";
     for (const auto& e: m_Param)
     {
-        std::string innerStr = "<Int16>" + std::to_string(e) + "</Int16>";
-         strParam += innerStr;
+        ss << "<Int16>" << std::to_string(e) << "</Int16>";
     }
-    strParam += "</ListOfInt16></value></param>";
-    return strParam;
+    ss << "</ListOfInt16></value></param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<uint16_t>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-    strParam += "<value><UInt16>" + std::to_string(m_Param) + "</UInt16></value>";
-    strParam += "</param>";
-    return strParam;
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><UInt16>" << std::to_string(m_Param) << "</UInt16></value>";
+    ss << "</param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<std::vector<uint16_t>>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-
-    strParam += "<value><ListOfUInt16>";
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><ListOfUInt16>";
     for (const auto& e: m_Param)
     {
-        std::string innerStr = "<UInt16>" + std::to_string(e) + "</UInt16>";
-         strParam += innerStr;
+        ss << "<UInt16>" << std::to_string(e) << "</UInt16>";
     }
-    strParam += "</ListOfUInt16></value></param>";
-    return strParam;
+    ss << "</ListOfUInt16></value></param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<int32_t>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-    strParam += "<value><Int32>" + std::to_string(m_Param) + "</Int32></value>";
-    strParam += "</param>";
-    return strParam;
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><Int32>" << std::to_string(m_Param) << "</Int32></value>";
+    ss << "</param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<std::vector<int32_t>>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-
-    strParam += "<value><ListOfInt32>";
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><ListOfInt32>";
     for (const auto& e: m_Param)
     {
-        std::string innerStr = "<Int32>" + std::to_string(e) + "</Int32>";
-         strParam += innerStr;
+        ss << "<Int32>" << std::to_string(e) << "</Int32>";
     }
-    strParam += "</ListOfInt32></value></param>";
-    return strParam;
+    ss << "</ListOfInt32></value></param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<uint32_t>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-    strParam += "<value><UInt32>" + std::to_string(m_Param) + "</UInt32></value>";
-    strParam += "</param>";
-    return strParam;
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><UInt32>" << std::to_string(m_Param) << "</UInt32></value>";
+    ss << "</param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<std::vector<uint32_t>>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-
-    strParam += "<value><ListOfUInt32>";
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><ListOfUInt32>";
     for (const auto& e: m_Param)
     {
-        std::string innerStr = "<UInt32>" + std::to_string(e) + "</UInt32>";
-         strParam += innerStr;
+        ss << "<UInt32>" << std::to_string(e) << "</UInt32>";
     }
-    strParam += "</ListOfUInt32></value></param>";
-    return strParam;
+    ss << "</ListOfUInt32></value></param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<int64_t>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-    strParam += "<value><Int64>" + std::to_string(m_Param) + "</Int64></value>";
-    strParam += "</param>";
-    return strParam;
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><Int64>" << std::to_string(m_Param) << "</Int64></value>";
+    ss << "</param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<std::vector<int64_t>>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-
-    strParam += "<value><ListOfInt64>";
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><ListOfInt64>";
     for (const auto& e: m_Param)
     {
-        std::string innerStr = "<Int64>" + std::to_string(e) + "</Int64>";
-         strParam += innerStr;
+        ss << "<Int64>" << std::to_string(e) << "</Int64>";
     }
-    strParam += "</ListOfInt64></value></param>";
-    return strParam;
+    ss << "</ListOfInt64></value></param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<uint64_t>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-    strParam += "<value><UInt64>" + std::to_string(m_Param) + "</UInt64></value>";
-    strParam += "</param>";
-    return strParam;
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><UInt64>" << std::to_string(m_Param) << "</UInt64></value>";
+    ss << "</param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<std::vector<uint64_t>>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-
-    strParam += "<value><ListOfUInt64>";
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><ListOfUInt64>";
     for (const auto& e: m_Param)
     {
-        std::string innerStr = "<UInt64>" + std::to_string(e) + "</UInt64>";
-         strParam += innerStr;
+        ss << "<UInt64>" << std::to_string(e) << "</UInt64>";
     }
-    strParam += "</ListOfUInt64></value></param>";
-    return strParam;
+    ss << "</ListOfUInt64></value></param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<float>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-    strParam += "<value><Float>" + std::to_string(m_Param) + "</Float></value>";
-    strParam += "</param>";
-    return strParam;
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><Float>" << std::to_string(m_Param) << "</Float></value>";
+    ss << "</param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<std::vector<float>>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-
-    strParam += "<value><ListOfFloat>";
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><ListOfFloat>";
     for (const auto& e: m_Param)
     {
-        std::string innerStr = "<Float>" + std::to_string(e) + "</Float>";
-         strParam += innerStr;
+        ss << "<Float>" << std::to_string(e) << "</Float>";
     }
-    strParam += "</ListOfFloat></value></param>";
-    return strParam;
+    ss << "</ListOfFloat></value></param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<double>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-    strParam += "<value><Double>" + std::to_string(m_Param) + "</Double></value>";
-    strParam += "</param>";
-    return strParam;
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><Double>" << std::to_string(m_Param) << "</Double></value>";
+    ss << "</param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<std::vector<double>>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-
-    strParam += "<value><ListOfDouble>";
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><ListOfDouble>";
     for (const auto& e: m_Param)
     {
-        std::string innerStr = "<Double>" + std::to_string(e) + "</Double>";
-         strParam += innerStr;
+        ss << "<Double>" << std::to_string(e) << "</Double>";
     }
-    strParam += "</ListOfDouble></value></param>";
-    return strParam;
+    ss << "</ListOfDouble></value></param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<bool>::get_parsed_string() const
 {
+    std::stringstream ss;
     std::string strBool = m_Param ? "true" : "false";
-
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-    strParam += "<value><Boolean>" + strBool + "</Boolean></value>";
-    strParam += "</param>";
-    return strParam;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><Boolean>" << strBool << "</Boolean></value>";
+    ss << "</param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<std::vector<bool>>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-
-    strParam += "<value><ListOfBoolean>";
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><ListOfBoolean>";
     for (const auto& e: m_Param)
     {
         std::string strBool = e ? "true" : "false";
-        std::string innerStr = "<Boolean>" + strBool + "</Boolean>";
-        strParam += innerStr;
+        ss << "<Boolean>" << strBool << "</Boolean>";
     }
-    strParam += "</ListOfBoolean></value></param>";
-    return strParam;
+    ss << "</ListOfBoolean></value></param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<std::string>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-    strParam += "<value><String>" + m_Param + "</String></value>";
-    strParam += "</param>";
-    return strParam;
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><String>" << m_Param << "</String></value>";
+    ss << "</param>";
+    return ss.str();
 }
 
 template<>
 std::string idds_xml_params_encode<std::vector<std::string>>::get_parsed_string() const
 {
-    std::string strParam = "<param>";
-    strParam += "<name>" + m_strParamName + "</name>";
-
-    strParam += "<value><ListOfString>";
+    std::stringstream ss;
+    ss << "<param>";
+    ss << "<name>" << m_strParamName << "</name>";
+    ss << "<value><ListOfString>";
     for (const auto& e: m_Param)
     {
-        std::string innerStr = "<String>" + e + "</String>";
-        strParam += innerStr;
+        ss << "<String>" << e << "</String>";
     }
-    strParam += "</ListOfString></value></param>";
-    return strParam;
+    ss << "</ListOfString></value></param>";
+    return ss.str();
 }
